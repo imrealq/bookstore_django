633aa49525b26f26440fc8f728fb554eda0e371b
TranQuynh9 <quynhtn29@gmail.com>
2021-03-17 00:51:14 -0700

    add database in setting and requirement

--- book_project/settings.py
+++ book_project/settings.py
@@ -74,15 +74,22 @@ WSGI_APPLICATION = "book_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# postgres heroku
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
-# mysql
+# sqlite3 local
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
+
+# mysql local
 
 # DATABASES = {
 #     'default': {
