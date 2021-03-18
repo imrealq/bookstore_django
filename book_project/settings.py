f85e8c1068dc4adba981ce333a4a059951dfbde4
TranQuynh9 <quynhtn29@gmail.com>
2021-03-17 21:45:55 -0700

    change key

--- book_project/settings.py
+++ book_project/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'ssjzk#)68oadzm#i%pcc2bs3jj)!cc-9&xnabbd0oy9b@8bwcf'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
