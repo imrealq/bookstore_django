3a822fe5334b3292cf3847e5230ef9bdf0586a9e
TranQuynh9 <quynhtn29@gmail.com>
2021-03-17 21:51:15 -0700

    change setting

--- book_project/settings.py
+++ book_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['book-9.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['book-8.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
